f00463255c5ec174ef521f076c86ca16be017d7f
Samuel <samuellgc@hotmail.com>
2022-05-21 12:19:01 -0300

    Criação navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <NavMenu />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import NavMenu from './components/NavMenu.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    NavMenu
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    margin: 0;
+    padding: 0;
+  }
 </style>
--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,17 +1,18 @@
 <template>
-  <section>
-    <div>
-      <span>Imagem</span>
-      <div>
-        <p>Surreal São Paulo</p>
-        <p>VTEX</p>
+  <nav class="nav-header">
+    <div class="nav-first">
+      <img src="../assets/logo.svg" alt="Logo empresa" class="logo-img" />
+      <div class="card-header">
+        <p class="p-sp">Surreal São Paulo</p>
+        <p class="p-vtex">VTEX</p>
       </div>
     </div>
-    <div>
-        <span>Ícone sino</span>
-        <p>Olá, Joaquim <span>Ícone usuário</span></p>
+    <div class="nav-last">
+      <img src="../assets/icone-bell.svg" alt="Logo empresa" class="i-sino" />
+      <p class="name-user">Olá, Joaquim</p>
+      <img src="../assets/usuario.svg" alt="Logo empresa" class="i-user" />
     </div>
-  </section>
+  </nav>
 </template>
 <script>
 export default {
@@ -19,4 +20,53 @@ export default {
 };
 </script>
 <style scoped>
+.nav-header,
+.nav-first,
+.nav-last {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 25px;
+}
+
+.nav-first,
+.nav-last {
+  height: 50px;
+  padding: 8px 20px;
+}
+
+.nav-header {
+  background-color: #9206e9;
+  color: white;
+  height: 70px;
+  padding: 5px 0;
+}
+
+.i-sino {
+  padding: 25px 23px;
+  border-right: 2px solid rgba(255, 255, 255, 0.2);
+}
+
+.card-header {
+  border: 2px solid white;
+  padding: 6px 130px 6px 10px;
+  border-radius: 10px;
+}
+
+.p-sp,
+.p-vtex,
+.name-user {
+  font-family: "Open Sans";
+}
+
+.p-sp,
+.name-user {
+  font-weight: 700;
+  size: 16px;
+}
+
+.p-vtex {
+  font-weight: 400;
+  size: 12px;
+}
 </style>
